da92002a61fc24325433b6f5c1963d6718c46577
Gabriel Venegas <94145724+GVenegas1@users.noreply.github.com>
2025-11-05 23:52:45 -0800

    STD

--- word_length_std_dev.py
+++ word_length_std_dev.py
@@ -6,7 +6,7 @@
 # Description: This program finds the standard deviation of the
 #length of words in a sentence.
 
-
+import string
 def word_length_std_dev(text):
     """
     This function takes the string (text) and returns the standard deviation
@@ -17,37 +17,33 @@ def word_length_std_dev(text):
     #split the text into words
     words = text.split()
 
-    #counting how many words there are
-    N = len(words)
-
-    #if there is only one or no words, STD is 0
-    if N <= 1:
-        return 0
-    # Makes a list with the length of each word
-    word_lengths = []
+    #Remove punctuation from each word
+    clean_words = []
     for word in words:
-        word_lengths.append(len(word))
+        clean_word=word.strip(string.punctuation)
+        if clean_word:
+            clean_words.append(clean_word)
 
-    #finds the average (mean)word length
-    total_length = sum(word_lengths)
-    mean = total_length / N
+    #if there is only one or more words, then std is 0
+    N = len(clean_words)
+    if N <= 1:
+        return 0.0
+
+    #Get length of eac word
+    word_lengths = [len(word) for word in clean_words]
+
+    #calculate the mean (avg) of length
+    mean = sum(word_lengths) / N
 
     #Finds how far each length is from the mean, then sq it
-    sq_diffs = []
-    for length in word_lengths:
-        difference = length - mean
-        squared = difference ** 2
-        sq_diffs.append(squared)
+    sq_diffs = [(length - mean) ** 2 for length in word_lengths]
 
-    #Add all the squared difference together
-    sum_sq_difference  = sum(sq_diffs)
-
-    #Divid by (N-1 and take the squared root
-    std_dev = (sum_sq_difference / N) ** 0.5
+    #STD
+    std_dev= (sum(sq_diffs) / (N - 1)) ** 0.5
 
     return std_dev
 
 #Test the function
-text= "elle when tp sleep early"
+text= "There is wisdom in turning as often as possible from the familiar to the unfamiliar it keeps the mind nimble it kills prejudice and it fosters humor"
 answer = word_length_std_dev(text)
 print("The STD is ", round(answer, 2))
